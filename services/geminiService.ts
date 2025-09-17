8557d8643d69a5c741c9c96f239919e154cd3c04
brandonleirer-lab <brandon.leirer@opusagency.com>
2025-09-17 11:44:04 -0500

    Update geminiService.ts - comment out the responeMimeType

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -24,7 +24,7 @@ export const generateImageVariation = async (
                         { text: prompt },
                     ],
                 },
-                config: { responseMimeType: "image/png" },
+                //config: { responseMimeType: "image/png" },
             }),
         });
 
